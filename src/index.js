16bd5b760d6df9e98c0cca743aa7feed37797536
Alexei Dvorac <lexhxas@gmail.com>
2016-08-13 16:11:06 +0300

    Save user data to mongoDB instead of txt.

--- src/index.js
+++ src/index.js
@@ -4,23 +4,36 @@ const co = require('co');
 const fs = require('fs');
 const cheerio = require('cheerio');
 
-const CONFIG = require('./config.json');
+const CONFIG = require('./config/config.json');
 const ph = require('./phantom_helpers.js');
+const db = require('./db.js');
+const collection = db.get('data');
+
 
 co(function*() {
     // const instance = yield * ph.initPhantomInstance();
     const url = CONFIG.url + CONFIG.user_id;
     const html = yield * ph.fetchPageContent(url, undefined, false);
     const $ = cheerio.load(html);
-    let text = CONFIG.parse_map.map(parseItem.bind(null, $)).join('\n') + '\n';
-    text += parseDetailedProfileInformation($);
-    text += parseCounters($);
+    const data = {
+        user_id: CONFIG.user_id
+    };
+    CONFIG.parse_map.forEach(item => {
+        const parsed = parseItem($, item);
+        data[parsed.key] = parsed.value;
+    });
+    const detailed_info = parseDetailedProfileInformation($);
+    const counters = parseCounters($);
+    console.log('Data parsed');
+    Object.assign(data, detailed_info, counters);
 
-    fs.writeFileSync(`${CONFIG.user_id}.txt`, text);
+    fs.writeFileSync(`../logs/${CONFIG.user_id}.json`, JSON.stringify(data));
+    collection.insert(data).then(() => db.close());
+
 });
 
 function parseItem($, item) {
-    let result = '';
+    let result = {};
     switch (item.type) {
     case 'text':
         result = parseTextItem($, item);
@@ -29,7 +42,7 @@ function parseItem($, item) {
         result = parseBooleanItem($, item);
         break;
     default:
-        console.log('No parser for type', item.type);
+        console.error('No parser for type', item.type);
 
     }
 
@@ -38,35 +51,47 @@ function parseItem($, item) {
 
 function parseTextItem($, item) {
     const text = $(item.selector).text();
-    return `${item.name}: ${text}`;
+    return {
+        key: item.name,
+        value: text
+    };
 }
 
 function parseBooleanItem($, item) {
     let text = $(item.selector).length > 0;
-    return `${item.name}: ${text}`;
+    return {
+        key: item.name,
+        value: text
+    };
 }
 
 function parseDetailedProfileInformation($) {
-    let text = '';
+    let data = {};
     $('.profile_info_block').each((i, el) => {
-        text += $(el).find('profile_info_header').text() + '\n';
+        // text += $(el).find('profile_info_header').text() + '\n';
         $(el).find('.profile_info_row').each((i, el) => {
             let title = $(el).find('.label').text();
             let content = $(el).find('.labeled').text();
-            text += `${title} ${content}\n`;
+            if (data[title]) {
+                console.error(`Conflicting data title <${title}>.\nExisting: ${data[title]}\nNew: ${content}`);
+            }
+            data[title] = content;
         });
     });
 
-    return text;
+    return data;
 }
 
 function parseCounters($) {
-    let text = '';
+    let data = {};
     $('.counts_module .page_counter').each((i, el) => {
         let title = $(el).find('.label').text();
         let content = $(el).find('.count').text();
-        text += `${title}: ${content}\n`;
+        if (data[title]) {
+            console.error(`Conflicting data title <${title}>.\nExisting: ${data[title]}\nNew: ${content}`);
+        }
+        data[title] = content;
     });
 
-    return text;
+    return data;
 }
