e78c677a156d8e657036ecdf6bc61a2cd4961851
Alexei Dvorac <lexhxas@gmail.com>
2016-08-15 18:49:06 +0300

    Apply extract method on scraper.scrape. Refactor scraper.js. Extract keys that are used to filter updates into config.json.

--- src/scraper.js
+++ src/scraper.js
@@ -12,6 +12,7 @@ const helpers = require('./helpers/helpers.js');
 const CONFIG = require('./config/config.json');
 const collection = db.get('data');
 const USER_ID = process.argv[2];
+let URL;
 let instance;
 
 module.exports = exports = {};
@@ -21,72 +22,106 @@ exports.work = function() {
         throw Error('No user ID supplied.');
     }
 
+    URL = CONFIG.url + USER_ID;
     scrape();
-    setInterval(scrape, CONFIG.interval * 1000);
 };
 
 
 function scrape() {
     co(function*() {
-        try{
+        const start_time = new Date();
+        const html = yield * getPageContent(URL);
+        const $ = cheerio.load(html);
+        const user_data = collectUserData($);
 
-            if (!instance) {
-                instance = yield * ph.initPhantomInstance();
-            }
-            console.log('Fetching data...');
-            const timestamp = new Date();
-            const url = CONFIG.url + USER_ID;
-            const html = yield * ph.fetchPageContent(url, instance, false);
-            const $ = cheerio.load(html);
-            const data = {
-                user_id: USER_ID,
-                timestamp: timestamp
-            };
-            CONFIG.parse_map.forEach(item => {
-                const parsed = parse(item.type, $, item);
-                data[parsed.key] = parsed.value;
-            });
-            const detailed_info = parse('detailedProfileInformation', $);
-            const counters = parse('counters', $);
-            Object.assign(data, detailed_info, counters);
-
-            let updates = yield * getDiff(data);
-            if (updates) {
-                console.log('THERE ARE UPDATES!');
-                yield collection.insert(data);
-                console.log('UPDATES were inserted in DB!');
-            }
-
-            helpers.clearConsole();
-            let formatted_data = format('dataForConsole', data);
-            if (updates && updates !== data) {
-                formatted_data += format('updatesForConsole', updates);
-            }
-            console.log(formatted_data);
-        } catch(err) {
-            console.log(err);
+        const user_updates = yield * checkUserDataForUpdates(user_data);
+        if (user_updates) {
+            yield collection.insert(user_data);
         }
+
+        const formatted_data = prepareConsoleOutput(user_data, user_updates);
+        helpers.clearConsole();
+        process.stdout.write(formatted_data);
+
+        const end_time = new Date();
+        const timeout_correction = end_time - start_time;
+        return timeout_correction;
+    })
+    .then(timeout_correction => {
+        const timeout = CONFIG.interval * 1000 - timeout_correction;
+        setTimeout(scrape, timeout);
+    })
+    .catch(err => {
+        console.log(err);
     });
 }
 
-function* getDiff(data) {
-    let updates = null;
-    let count = yield collection.count({user_id: USER_ID});
-    console.log(count);
+function* getPageContent(url) {
+    if (!instance) {
+        instance = yield * ph.initPhantomInstance();
+    }
+    process.stdout.write('\nFetching data...');
+    const html = yield * ph.fetchPageContent(url, instance, false);
+
+    return html;
+}
+
+function collectUserData($) {
+    const data = {
+        user_id: USER_ID,
+        timestamp: new Date()
+    };
+
+    CONFIG.parse_map.forEach(item => {
+        const parsed = parse(item.type, $, item);
+        data[parsed.key] = parsed.value;
+    });
+
+    const detailed_info = parse('detailedProfileInformation', $);
+    const counters = parse('counters', $);
+    Object.assign(data, detailed_info, counters);
+
+    return data;
+}
+
+function* checkUserDataForUpdates(data) {
+    let count = yield collection.count({
+        user_id: USER_ID
+    });
+
     if (!count) {
         // No entries for this USER_ID yet
-        console.log('FUCKING got here');
         return data;
     }
-    let cursor = yield collection.find({user_id: USER_ID}, {rawCursor: true});
-    let last_document = yield cursor.sort([['timestamp', -1]]).limit(1).toArray();
+
+    const last_document = yield * getLastUserDocument();
+    const updates = getDiff(last_document, data);
+
+    return updates;
+}
+
+function* getLastUserDocument() {
+    let cursor = yield collection.find({
+        user_id: USER_ID
+    }, {
+        rawCursor: true
+    });
+
+    let last_document = yield cursor.sort([
+        ['timestamp', -1]
+    ]).limit(1).toArray();
     last_document = last_document[0];
-    let keys = Object.keys(data).filter(k => ['timestamp', 'Last seen'].indexOf(k) === -1);
+
+    return last_document;
+}
+
+function getDiff(last_document, data) {
+    let updates = {};
+
+    let excluded = CONFIG.keys_to_exclude_when_looking_for_updates.concat(['timestamp']);
+    let keys = Object.keys(data).filter(k => excluded.indexOf(k) === -1);
     for (let k of keys) {
         if (data[k] !== last_document[k]) {
-            if (!updates) {
-                updates = {};
-            }
             updates[k] = {
                 old: last_document[k],
                 current: data[k]
@@ -94,5 +129,23 @@ function* getDiff(data) {
         }
     }
 
-    return updates;
+    filterUpdates(updates);
+    if (Object.keys(updates).length) {
+        return updates;
+    }
+
+    return null;
+}
+
+function filterUpdates(updates) {
+    CONFIG.keys_to_hide_from_updates.forEach(k => delete updates[k]);
+}
+
+function prepareConsoleOutput(data, updates) {
+    let formatted_data = format('dataForConsole', data);
+    if (updates && updates !== data) {
+        formatted_data += format('updatesForConsole', updates);
+    }
+
+    return formatted_data;
 }
