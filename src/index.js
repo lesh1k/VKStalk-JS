6dfca05de99d56f27c321bdcf02dcbf3d567fa69
Alexei Dvorac <lexhxas@gmail.com>
2016-08-15 13:47:08 +0300

    Check for changes in scraped data versus last DB entry. Insert data in DB only if there are changes.

--- src/index.js
+++ src/index.js
@@ -21,32 +21,45 @@ setInterval(scrape, CONFIG.interval * 1000);
 
 function scrape() {
     co(function*() {
-        if (!instance) {
-            instance = yield * ph.initPhantomInstance();
+        try{
+
+            if (!instance) {
+                instance = yield * ph.initPhantomInstance();
+            }
+            console.log('Fetching data...');
+            const timestamp = new Date();
+            const url = CONFIG.url + USER_ID;
+            const html = yield * ph.fetchPageContent(url, instance, false);
+            const $ = cheerio.load(html);
+            const data = {
+                user_id: USER_ID,
+                timestamp: timestamp
+            };
+            CONFIG.parse_map.forEach(item => {
+                const parsed = parseItem($, item);
+                data[parsed.key] = parsed.value;
+            });
+            const detailed_info = parseDetailedProfileInformation($);
+            const counters = parseCounters($);
+            Object.assign(data, detailed_info, counters);
+
+            fs.writeFileSync(`../logs/${USER_ID}.json`, JSON.stringify(data));
+
+            let updates = yield * getDiff(data);
+            if (updates) {
+                console.log('THERE ARE UPDATES!');
+                yield collection.insert(data);
+            }
+
+            // clearConsole();
+            let formatted_data = format(data, 'console');
+            if (updates && updates !== data) {
+                formatted_data += formatUpdates(updates);
+            }
+            console.log(formatted_data);
+        } catch(err) {
+            console.log(err);
         }
-        console.log('Fetching data...');
-        const timestamp = new Date();
-        const url = CONFIG.url + USER_ID;
-        const html = yield * ph.fetchPageContent(url, instance, false);
-        const $ = cheerio.load(html);
-        const data = {
-            user_id: USER_ID,
-            timestamp: timestamp
-        };
-        CONFIG.parse_map.forEach(item => {
-            const parsed = parseItem($, item);
-            data[parsed.key] = parsed.value;
-        });
-        const detailed_info = parseDetailedProfileInformation($);
-        const counters = parseCounters($);
-        Object.assign(data, detailed_info, counters);
-
-        fs.writeFileSync(`../logs/${USER_ID}.json`, JSON.stringify(data));
-        collection.insert(data);
-
-        clearConsole();
-        const formatted_data = format(data, 'console');
-        console.log(formatted_data);
     });
 }
 
@@ -134,16 +147,54 @@ function format(data, purpose) {
 }
 
 function formatForConsole(data) {
+    let name = data.Name.split(' ').map(s => s.trim()).join(' ');
     let result = `App launched on ${LAUNCH_DATE}\n`;
-    result += `User name: ${data.Name}\n`;
+    result += `User name: ${name}\n`;
     result += `User ID: ${data.user_id}\n\n`;
     result += `>>> Checked on ${data.timestamp} <<<\n\n`;
-    result += `${data.Name} -- ${data['Last seen']}`;
+    result += `${name} -- ${data['Last seen']}`;
     if (data.isFromMobile) {
         result += ' [Mobile]';
     }
     result += '\n';
     result += `Current status: ${data['Current status']}\n`;
+
+    return result;
+}
+
+function* getDiff(data) {
+    let updates = null;
+    let count = yield collection.count({user_id: USER_ID});
+    console.log(count);
+    if (!count) {
+        // No entries for this USER_ID yet
+        console.log('FUCKING got here');
+        return data;
+    }
+    let cursor = yield collection.find({user_id: USER_ID}, {rawCursor: true});
+    let last_document = yield cursor.sort([['timestamp', -1]]).limit(1).toArray();
+    last_document = last_document[0];
+    let keys = Object.keys(data).filter(k => ['timestamp', 'Last seen'].indexOf(k) === -1);
+    for (let k of keys) {
+        if (data[k] !== last_document[k]) {
+            if (!updates) {
+                updates = {};
+            }
+            updates[k] = {
+                old: last_document[k],
+                current: data[k]
+            };
+        }
+    }
+
+    return updates;
+}
+
+function formatUpdates(updates) {
+    let result = '\nUPDATES\n';
+    for (let k in updates) {
+        result += `${k}: ${updates[k].current}\n`;
+    }
 
     return result;
 }
