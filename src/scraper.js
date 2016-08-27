f06ff4647dde8f5cdb78ae139a1663e7ea483f98
Alexei Dvorac <lexhxas@gmail.com>
2016-08-27 16:46:12 +0300

    Add logging to scrape(). Replace process.exit(), in case !isUserPageOpen($) is true, with return, to retry scraping after timeout.

--- src/scraper.js
+++ src/scraper.js
@@ -9,11 +9,13 @@ const parse = require('./parser.js');
 const format = require('./format.js');
 const helpers = require('./helpers/helpers.js');
 const db_helpers = require('./helpers/db_helpers.js');
+const logger = require('./logger.js');
 
 const CONFIG = require('./config/config.json');
 const collection = db.get('data');
 let USER_ID = null;
 let logs_written = 0;
+let retry_count = 0;
 let URL;
 let instance;
 
@@ -31,29 +33,57 @@ exports.work = function(user_id) {
 
 
 function scrape() {
+    logger.debug('Function call. scrape()', {args: [].slice.call(arguments)});
+    logger.info('Start scraping', {user_id: USER_ID});
     co(function*() {
+        logger.info('Fetch HTML', {user_id: USER_ID});
         const html = yield * getPageContent(URL);
         const $ = cheerio.load(html);
         if (!isUserPageOpen($)) {
-            helpers.terminate('Cannot scrape page', 'Profile is either hidden, not existing or deleted');
+            logger.error('Cannot scrape page. !isUserPageOpen($) == true', {user_id: USER_ID, url: URL});
+            logger.info('Scrape round skipped. Retry after timeout', {user_id: USER_ID});
+            helpers.clearConsole();
+
+            let message = '';
+            message += `Attempt #${++retry_count}\n`;
+            message += 'Cannot scrape page.';
+            message += 'Profile is either hidden, not existing or deleted\n';
+            message += `Please check that USER_ID=${USER_ID} and URL=${URL} are correct.\n`;
+            message += 'Retry after timeout.';
+
+            console.log(message);
+            return;
+            // helpers.terminate('Cannot scrape page', 'Profile is either hidden, not existing or deleted');
         }
+        retry_count = 0;
+
+        logger.info('Extract user data from fetched HTML', {user_id: USER_ID});
         const user_data = collectUserData($);
 
+        logger.info('Check if new data has updates', {user_id: USER_ID, new_user_data: user_data});
         const user_updates = yield * checkUserDataForUpdates(user_data);
         if (user_updates) {
+            logger.info('Write user data to DB', {user_id: USER_ID, data: user_data, updates: user_updates});
             yield collection.insert(user_data);
             logs_written++;
         }
 
+        logger.info('Format data for console output', {user_id: USER_ID, data: user_data});
         const formatted_data = prepareConsoleOutput(user_data, user_updates);
         helpers.clearConsole();
+        logger.debug('Write formatted data to console', {user_id: USER_ID, formatted_data: formatted_data});
         process.stdout.write(formatted_data);
     })
     .then(() => {
         const timeout = CONFIG.interval * 1000;
+        logger.debug('Set timeout for next scrape() call.', {user_id: USER_ID, timeout: timeout});
+        if (retry_count) {
+            console.log(`Timeout ${timeout / 1000} seconds`);
+        }
         setTimeout(scrape, timeout);
     })
     .catch(err => {
+        logger.error('Caught exception in scrape()', {user_id: USER_ID, exception: err});
         console.log(err);
     });
 }
@@ -62,7 +92,7 @@ function* getPageContent(url) {
     if (!instance) {
         instance = yield * ph.initPhantomInstance();
     }
-    process.stdout.write('\nFetching data...');
+    console.log('\nFetching data...');
     const html = yield * ph.fetchPageContent(url, instance, false);
 
     return html;
