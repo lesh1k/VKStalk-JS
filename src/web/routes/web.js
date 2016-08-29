d16ff93fb61941de458d41ba018381c78bfc71a4
Alexei Dvorac <lexhxas@gmail.com>
2016-08-30 02:42:10 +0300

    Handle request for report on user, by sending JSON data generated by reportMusic().

--- src/web/routes/web.js
+++ src/web/routes/web.js
@@ -2,9 +2,25 @@
 
 const router = require('express').Router();
 
+const report = require('../../stalker/report.js');
+
 module.exports = router;
 
 
-router.get('/', (req, res) => {
-    res.sendFile('index.html');
-});
+router.route('/')
+    .get((req, res) => {
+        res.sendFile('index.html');
+    })
+    .post((req, res) => {
+        const action = req.body['action'];
+        const user_id = req.body['user-id'];
+
+        if (action === 'report') {
+            report('music', user_id)
+                .then(data => {
+                    res.json(data);
+                });
+        } else {
+            res.send(`Requested to ${action} on ${user_id}`);
+        }
+    });
