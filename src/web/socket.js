3976df97a7b244f12f3a6f5ca4f4236ba4f4f219
Alexei Dvorac <lexhxas@gmail.com>
2016-08-30 23:09:47 +0300

    Socket, listen to existing worker for user_id on new connections, if worker for user_id exists.

--- src/web/socket.js
+++ src/web/socket.js
@@ -6,21 +6,29 @@ const path = require('path');
 
 module.exports = function(server) {
     const socket_io = require('socket.io')(server);
-    const workers = {};
+    const workers = {}; // {user_id: worker_id}
 
     socket_io.on('connection', (socket) => {
         socket.emit('stalk-data', 'Connecting...');
         socket.emit('connection', 'Done!');
 
         socket.on('stalk-request', user_id => {
-            cluster.setupMaster({
-                exec: path.resolve(__dirname, '../vkstalk'),
-                args: ['stalk', user_id],
-                silent: true
-            });
+            const worker_id = workers[user_id];
+            let worker = cluster.workers[worker_id];
+            if (typeof worker_id === 'undefined') {
+                console.log('Creating new WORKER.');
+                cluster.setupMaster({
+                    exec: path.resolve(__dirname, '../vkstalk'),
+                    args: ['stalk', user_id],
+                    silent: true
+                });
 
-            let worker = cluster.fork();
-            workers[user_id] = worker.id;
+                worker = cluster.fork();
+                workers[user_id] = worker.id;
+            } else {
+                console.log('Receiving info from an existing worker');
+            }
+
             worker.process.stdout.on('data', buffer => {
                 socket.emit('stalk-data', buffer.toString('utf8'));
             });
