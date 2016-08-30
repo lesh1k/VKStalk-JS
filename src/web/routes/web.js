94b66ff82de52904ab8bfb9b12c6bd1dfd924093
Alexei Dvorac <lexhxas@gmail.com>
2016-08-30 05:31:10 +0300

    Add stalk page where live output from stalker can be viewed. Update res.sendFile by specifying root.

--- src/web/routes/web.js
+++ src/web/routes/web.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
 const report = require('../../stalker/report.js');
 
@@ -9,18 +10,29 @@ module.exports = router;
 
 router.route('/')
     .get((req, res) => {
-        res.sendFile('index.html');
+        res.sendFile('index.html', {root: __dirname + '/../views'});
     })
     .post((req, res) => {
         const action = req.body['action'];
         const user_id = req.body['user-id'];
 
-        if (action === 'report') {
-            report('music', user_id)
-                .then(data => {
-                    res.json(data);
-                });
-        } else {
-            res.send(`Requested to ${action} on ${user_id}`);
+        switch (action) {
+            case 'report':
+                report('music', user_id)
+                    .then(data => {
+                        res.json(data);
+                    });
+                break;
+            case 'stalk':
+                res.redirect(`/stalk/${user_id}`);
+                break;
+            default:
+                res.send(`Requested to ${action} on ${user_id}`);
+                break;
         }
+    });
+
+router.route('/stalk/:user_id')
+    .get((req, res) => {
+        res.sendFile('stalk.html', {root: __dirname + '/../views'});
     });
