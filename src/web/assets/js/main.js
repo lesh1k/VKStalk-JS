dd226a83f6770602e94d59c5fd8e2089a21a1275
Alexei Dvorac <lexhxas@gmail.com>
2016-09-05 23:16:55 +0300

    Enable socket rooms per stalked user ID. Allow enabling stalkers from the web interface.

--- src/web/assets/js/main.js
+++ src/web/assets/js/main.js
@@ -1,4 +1,5 @@
 /* eslint-env jquery */
+/* global io */
 
 (function() {
     'use strict';
@@ -13,3 +14,61 @@
         });
     });
 })();
+
+(function(socket_io) {
+    'use strict';
+    var STALKED_IDS = getStalkedIds();
+    console.log(STALKED_IDS);
+
+    setupSocket();
+
+    function setupSocket() {
+        var socket = socket_io.connect(window.location.origin);
+
+        socket.on('connection', function(data) {
+            console.log(data);
+            STALKED_IDS.forEach(function(stalked_id) {
+                console.log('Emitting join room', stalked_id);
+                socket.emit('join-room', stalked_id);
+            });
+        });
+
+        socket.on('stalk-data', function(data) {
+            console.log(data);
+            var card = document.getElementById(data.stalked_id);
+            card.querySelector('.stalk-data').innerText = data.message;
+        });
+
+        socket.on('disconnect', function() {
+            $('.stalk-data').text('Disconnected...');
+        });
+
+        setupStalkButton(socket);
+        setupStopButton(socket);
+    }
+
+    function setupStalkButton(socket) {
+        var $btns = $('.stalk-user');
+
+        $btns.click(function() {
+            var stalked_id = $(this).closest('.card').attr('id');
+            socket.emit('stalk-request', stalked_id);
+        });
+    }
+
+    function setupStopButton(socket) {
+        // var $btns = $('.stop-worker');
+        //
+        // $btns.click(function() {
+        //     var path_parts = window.location.pathname.split('/');
+        //     var user_id = path_parts[path_parts.length - 1];
+        //     socket.emit('stop-worker', user_id);
+        // });
+    }
+
+    function getStalkedIds() {
+        return $('.stalking-card').toArray().map(function(card) {
+            return $(card).attr('id');
+        });
+    }
+})(io);
--- src/web/socket.js
+++ src/web/socket.js
@@ -6,31 +6,52 @@ const path = require('path');
 
 module.exports = function(server) {
     const socket_io = require('socket.io')(server);
-    const workers = {}; // {user_id: worker_id}
+    const workers = {}; // {stalked_id: worker_id}
 
     socket_io.on('connection', (socket) => {
-        socket.emit('stalk-data', 'Connecting...');
-        socket.emit('connection', 'Done!');
+        socket.emit('connection', {message: 'Connection established.'});
+        socket.on('join-room', room => {
+            const stalked_id = room;
 
-        socket.on('stalk-request', user_id => {
-            const worker_id = workers[user_id];
+            console.log(`Joining room ${room}`);
+            socket.join(room, err => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+
+            socket_io.sockets.to(room).emit('stalk-data', {stalked_id: stalked_id, message:'Connecting...'});
+
+            const worker_id = workers[stalked_id];
+            let worker = cluster.workers[worker_id];
+            if (!worker) {
+                socket_io.sockets.to(room).emit('stalk-data', {stalked_id: stalked_id, message: 'Stalker offline. Click "STALK" to turn it on.'});
+            } else {
+                worker.process.stdout.on('data', buffer => {
+                    socket_io.sockets.to(room).emit('stalk-data', {stalked_id: stalked_id, message: buffer.toString('utf8')});
+                });
+            }
+        });
+
+        socket.on('stalk-request', stalked_id => {
+            const worker_id = workers[stalked_id];
             let worker = cluster.workers[worker_id];
             if (!worker) {
                 console.log('Creating new WORKER.');
                 cluster.setupMaster({
                     exec: path.resolve(__dirname, '../vkstalk'),
-                    args: ['stalk', user_id],
+                    args: ['stalk', stalked_id],
                     silent: true
                 });
 
                 worker = cluster.fork();
-                workers[user_id] = worker.id;
+                workers[stalked_id] = worker.id;
             } else {
                 console.log('Receiving info from an existing worker');
             }
 
             worker.process.stdout.on('data', buffer => {
-                socket.emit('stalk-data', buffer.toString('utf8'));
+                socket_io.sockets.to(stalked_id).emit('stalk-data', {stalked_id: stalked_id, message: buffer.toString('utf8')});
             });
         });
 
@@ -38,8 +59,8 @@ module.exports = function(server) {
         //     Object.keys(cluster.workers).forEach(id => cluster.workers[id].kill());
         // });
 
-        socket.on('stop-worker', user_id => {
-            const worker_id = workers[user_id];
+        socket.on('stop-worker', stalked_id => {
+            const worker_id = workers[stalked_id];
             cluster.workers[worker_id].kill();
             socket.disconnect();
         });
