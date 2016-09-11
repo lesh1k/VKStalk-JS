469f116c96c5c590fbbb2fb56444914be7bd2d82
Alexei Dvorac <lexhxas@gmail.com>
2016-09-11 22:28:34 +0300

    Remove unused helpers from console interface.

--- src/console/helpers/helpers.js
+++ src/console/helpers/helpers.js
@@ -34,35 +34,6 @@ exports.capitalize = function(str) {
     return str;
 };
 
-exports.convertTimeTo24hrs = function(time) {
-    logger.debug('Function call. convertTimeTo24hrs(time)', {args: [].slice.call(arguments)});
-    let parts = time.split(' ');
-    let hours = parts[0].split(':')[0];
-    let minutes = parts[0].split(':')[1];
-    let period = parts[1];
-
-    if (period === 'am') {
-        if (hours.length === 1) {
-            hours = '0' + hours;
-        } else if (hours === '12') {
-            hours = '00';
-        }
-    } else {
-        if (hours !== '12') {
-            hours = String(parseInt(hours, 10) + 12);
-        }
-    }
-
-    return `${hours}:${minutes}`;
-};
-
-exports.terminate = function(reason, message) {
-    logger.debug('Function call. terminate(reason, message)', {args: [].slice.call(arguments)});
-    logger.error('Manual call of process.exit()', {reason: reason, message: message});
-    console.log(`\n\nProcess exited\nReason: ${reason}\nMessage: ${message}`);
-    process.exit();
-};
-
 exports.monitorMemoryLeaks = function() {
     memwatch.on('leak', function(info) {
         logger.warn('Possible MEMORY LEAK detected', info);
