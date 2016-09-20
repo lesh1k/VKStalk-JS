e754de3af0c88756cc07429dbac7eca1fcd50ed2
Alexei Dvorac <lexhxas@gmail.com>
2016-09-21 00:54:59 +0300

    Remove commented code

--- src/stalker/helpers/db_helpers.js
+++ src/stalker/helpers/db_helpers.js
@@ -5,17 +5,4 @@ module.exports = exports = {};
 exports.getLastUserDocument = function* (collection, user_id) {
     const doc = yield collection.findOne({user_id: user_id}, {sort: {timestamp: -1}});
     return doc;
-
-    // let cursor = yield collection.find({
-    //     user_id: user_id
-    // }, {
-    //     rawCursor: true
-    // });
-    //
-    // let last_document = yield cursor.sort([
-    //     ['timestamp', -1]
-    // ]).limit(1).toArray();
-    // last_document = last_document[0];
-    //
-    // return last_document;
 };
